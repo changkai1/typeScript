cd7f32626e1da0125c20bb5db20847e12fcd57f0
changkai <18562122962@163.com>
2021-05-24 17:48:24 +0800

    清空

--- 01_ts自动编译/index.ts
+++
@@ -1,7 +0,0 @@
-(() => {
-    function sayHi(str) {
-        return str;
-    }
-    let str = "小甜甜";
-    console.log(sayHi(str))
-})()
--- 01_ts自动编译/js/index.js
+++
@@ -1,7 +0,0 @@
-(function () {
-    function sayHi(str) {
-        return str;
-    }
-    var str = "小甜甜";
-    console.log(sayHi(str));
-})();
